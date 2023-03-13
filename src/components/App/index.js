3010e363f0046fd74a6bc0a3c28aa3d9c2abe432
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-13 17:41:10 +0100

    git commit 'integratino of react-router-dom then navigation elements'

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,28 @@
 import '../../App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../Header';
 import Landing from '../Landing';
 import Footer from '../Footer';
+import Welcome from '../Welcome';
+import Login from '../Login';
+import Signup from '../Signup';
+import ErrorPage from '../ErrorPage';
 
 function App() {
 	return (
-		<div>
-			<Header />
-			<Landing />
-			<Footer />
-		</div>
+		<Router>
+        <Header />
+
+        <Routes>
+			<Route path="/" element={<Landing />} />
+			<Route path="/welcome" element={<Welcome />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="*" element={<ErrorPage />} />
+        </Routes>
+
+        <Footer />
+    </Router>
 	);
 }
 
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,11 @@
+const Welcome = () => {
+    return ( 
+        <div className='quiz-bg'>
+            <div className="container">
+
+            </div>
+        </div>
+    );
+}
+
+export default Welcome;
