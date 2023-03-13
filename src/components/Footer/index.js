cd6bd7033d262b52319f383fda28c5ed9636ae5f
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-13 14:36:58 +0100

    git commit '119-part'

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Forerunner</p>
+                <p>Les icônes Wolverine, Iron-Man, Spider-Man et Batman sont prises sur iconFinder.com</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer
