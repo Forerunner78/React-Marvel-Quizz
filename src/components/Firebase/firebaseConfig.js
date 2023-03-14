608e48f33593428afc3d09be9db38e2ad11514d9
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-14 10:15:41 +0100

    ggit commit 'integration of Firebase'

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyDH8jRUSGgjnCzmL_tH0cv2hixRQ1dLwmY",
+    authDomain: "marvel-quiz-51916.firebaseapp.com",
+    projectId: "marvel-quiz-51916",
+    storageBucket: "marvel-quiz-51916.appspot.com",
+    messagingSenderId: "36637033744",
+    appId: "1:36637033744:web:ac27e1e377c6f323599195"
+};
+
+const app = initializeApp(config);
