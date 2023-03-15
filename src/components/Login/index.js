d4e0cede7128f7fedb1d4d9badcdd75350c5803b
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-15 18:05:35 +0100

    Integration of password reset

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -71,6 +71,8 @@ const Login = () => {
                         </form>
                         <div className="linkContainer">
                             <Link className="simpleLink" to="/signup">Nouveau sur Marvel Quiz ? Inscrivez-vous maintenant.</Link>
+                            <br />
+                            <Link className="simpleLink" to="/forgetpassword">Mot de passe oublié? Réinitialisez le ici.</Link>/Link>
                         </div>
                     </div>
                 </div>
