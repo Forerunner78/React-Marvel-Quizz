342d36aa9e010d21128ff85c740c1946ada86650
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-15 17:16:51 +0100

    User session implementation and indentation

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,15 +1,35 @@
-import Logout from "../Logout";
-import Quiz from "../Quiz";
+// Firebase versions 7, 8 et "9" en POO
+import { useState, Fragment, useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../Firebase/firebaseConfig'
+import { useNavigate } from 'react-router-dom'
+import Logout from '../Logout'
+import Quiz from '../Quiz'
 
 const Welcome = () => {
-    return ( 
-        <div className='quiz-bg'>
+    const [userSession, setUserSession] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const listener = onAuthStateChanged(auth, user => {
+            user ? setUserSession(user) : navigate('/')
+        })
+        return listener;
+    }, [])
+
+    return userSession === null ? (
+        <Fragment>
+            <div className="loader"></div>
+            <p className="loaderText">Loading ...</p>
+        </Fragment>
+    ) : (
+        <div className="quiz-bg">
             <div className="container">
                 <Logout />
                 <Quiz />
             </div>
         </div>
-    );
+    )
 }
 
-export default Welcome;
+export default Welcome
