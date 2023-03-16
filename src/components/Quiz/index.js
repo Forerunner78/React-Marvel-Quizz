9dab85a597e891f2b00a93955702c228fada0761
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-16 15:32:43 +0100

    Take back the pseudo with the database

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,13 +1,20 @@
+import { Component } from "react";
 
+class Quiz extends Component {
+    render() {
 
-const Quiz = () => {
-    return ( 
-        <div>
+        const {pseudo} = this.props.userData;
+        return ( 
             <div>
-                
+                <div>
+                    <h2>
+                        Pseudo: {pseudo}
+                    </h2>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
 }
 
 export default Quiz;
