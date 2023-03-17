aa79098ff063e29ece922ce07d914b6452de5ab3
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-17 10:50:17 +0100

    Quiz work and indentation with Prettier

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,17 +1,76 @@
 import { Component } from "react";
+import { QuizMarvel } from "../quizMarvel";
 import Levels from "../Levels";
 import ProgressBar from "../Progressbar";
 
 class Quiz extends Component {
+    getLevelNames = () => {
+        const quizLevels = Object.keys(QuizMarvel[0].quizz).reduce(
+            (acc, key) => {
+                acc.push(key);
+                return acc;
+            },
+            []
+        );
+        return quizLevels;
+    };
+
+    loadQuestions = (quizz) => {
+        const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz];
+        if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+            const newArray = fetchedArrayQuiz.map(
+                ({ answer, ...keepRest }) => keepRest
+            );
+            this.setState({
+                storedQuestions: newArray,
+            });
+        } else {
+            console.log("Pas assez de questions!");
+        }
+    };
+
+    componentDidMount() {
+        this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.storedQuestions !== prevState.storedQuestions) {
+            this.setState({
+                question:
+                    this.state.storedQuestions[this.state.idQuestion].question,
+                options:
+                    this.state.storedQuestions[this.state.idQuestion].options,
+            });
+        }
+    }
+
+    state = {
+        levelNames: this.getLevelNames(),
+        quizLevel: 0,
+        maxQuestions: 10,
+        storedQuestions: [],
+        question: null,
+        options: [],
+        idQuestion: 0,
+    };
+
     render() {
-        return ( 
+        const displayOptions = this.state.options.map((option, index) => {
+            return (
+                <p key={index} className="answerOptions">
+                    {option}
+                </p>
+            );
+        });
+        return (
             <div>
                 <Levels />
                 <ProgressBar />
+                <h2>{this.state.question}</h2>
+                {displayOptions}
             </div>
         );
     }
-    
 }
 
 export default Quiz;
