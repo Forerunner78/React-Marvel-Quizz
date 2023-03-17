7066fa3a56adb8026d9db10b2ac59be4e4eac84c
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-17 10:50:17 +0100

    Quiz work and indentation with Prettier

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,31 +1,31 @@
-import '../../App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from '../Header';
-import Landing from '../Landing';
-import Footer from '../Footer';
-import Welcome from '../Welcome';
-import Login from '../Login';
-import Signup from '../Signup';
-import ErrorPage from '../ErrorPage';
-import ForgetPassword from '../ForgetPassword';
+import "../../App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../Header";
+import Landing from "../Landing";
+import Footer from "../Footer";
+import Welcome from "../Welcome";
+import Login from "../Login";
+import Signup from "../Signup";
+import ErrorPage from "../ErrorPage";
+import ForgetPassword from "../ForgetPassword";
 
 function App() {
-	return (
-		<Router>
-			<Header />
+    return (
+        <Router>
+            <Header />
 
-			<Routes>
-				<Route exact path="/" element={<Landing />} />
-				<Route path="/welcome" element={<Welcome />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
-				<Route path="/forgetpassword" element={<ForgetPassword />} />
-				<Route path="*" element={<ErrorPage />} />
-			</Routes>
+            <Routes>
+                <Route exact path="/" element={<Landing />} />
+                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/forgetpassword" element={<ForgetPassword />} />
+                <Route path="*" element={<ErrorPage />} />
+            </Routes>
 
-			<Footer />
-		</Router>
-	);
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
